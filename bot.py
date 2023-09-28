1f10cd7f0857d2b7fbb32ec3679629e9efcf2985
fede <niemil9326@gmail.com>
2023-09-28 14:47:25 -0300

    actions daily_host and report

--- bot.py
+++ bot.py
@@ -1,20 +1,28 @@
+import random
 import discord
 import local_settings
 from git_repo import get_merge_list
 
 starting_command = "/deploy"
 
+devs = ['Fede, Mati S, Mati M, Claudio', 'Valen', 'Paula', 'Jose', 'Franco',
+        'Gianluca', 'Benja', 'Rodri', 'Pablo', 'Seba']
+
 actions_msg = {
-    "_online":"Yes, I'm here and available to assist you. How can I help you today?",
-    "_list_backend": get_merge_list()
+    "_online": "Yes, I'm here and available to assist you. How can I help you today?",
+    "_daily_host": random.choice(devs),
+    "_report": get_merge_list()
 }
 
+
 def list_commands(commands_dict):
-    commands = "Here's the list of avalible commands:" + '\n'
+    commands = "I don't recognize that command, sorry." + \
+        "\n" + "Here's the list of avalible commands:" + "\n"
     for key in commands_dict.keys():
-        commands = commands + starting_command + str(key) + '\n'
-    
+        commands = commands + starting_command + str(key) + "\n"
+
     return commands
+
 
 def get_response(user_message: str) -> str:
     message = user_message.lower()
@@ -23,11 +31,9 @@ def get_response(user_message: str) -> str:
 
     if action in actions_msg:
         return actions_msg[action]
-    
-    if action == "_commands":
-        return list_commands(actions_msg)
-					
-    return "I don't recognize that command, sorry."
+
+    return list_commands(actions_msg)
+
 
 async def send_message(message, user_message, is_private):
     try:
@@ -37,35 +43,31 @@ async def send_message(message, user_message, is_private):
     except Exception as e:
         print(e)
 
+
 def run_bot():
-	TOKEN = local_settings.LOCAL_TOKEN
-	intents = discord.Intents.default()
-	intents.message_content = True
-	client = discord.Client(intents=intents)
+    TOKEN = local_settings.LOCAL_TOKEN
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
 
-	@client.event
-	async def on_ready():
-		print(f'{client.user} is now running!')
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
 
-	@client.event
-	async def on_message(message):
-		# Previene la ejecucion de la respuesta si el autor del mensaje es el mismo bot
-		if message.author == client.user:
-			return
-		
-		# Solo escucha mensajes que empiezen con /e
-		user_message = str(message.content)
-		if not user_message.startswith(starting_command):
-			return
-        
-		username = str(message.author)
-		channel = str(message.channel)
-		print(f"{username} said: '{user_message}' ({channel})")
-		await send_message(message, user_message,  is_private=False)
+    @client.event
+    async def on_message(message):
+        # Previene la ejecucion de la respuesta si el autor del mensaje es el mismo bot
+        if message.author == client.user:
+            return
 
-	client.run(TOKEN)
+        # Solo escucha mensajes que empiezen con /e
+        user_message = str(message.content)
+        if not user_message.startswith(starting_command):
+            return
 
-	
+        username = str(message.author)
+        channel = str(message.channel)
+        print(f"{username} said: '{user_message}' ({channel})")
+        await send_message(message, user_message,  is_private=False)
 
-
-
+    client.run(TOKEN)
--- git_repo.py
+++ git_repo.py
@@ -3,19 +3,30 @@ import local_settings
 
 mrs_to_show = 5
 
-def get_merge_list() -> str:
-    print("el repo es", local_settings.LOCAL_REPO)
-    repo = git.Repo(local_settings.LOCAL_REPO)
-    merge_commits = repo.iter_commits('HEAD', max_count=mrs_to_show, merges=True)
-    # Extract and print the branch names from the merge commit messages
-    merge_list = f"Showing the last {mrs_to_show} merge requests:" + '\n' + '\n'
 
-    for commit in merge_commits:
+def get_merge_list() -> str:
+    frontend = git.Repo(local_settings.LOCAL_REPO_FRONT)
+    merge_commits_front = frontend.iter_commits(
+        'HEAD', max_count=mrs_to_show, merges=True)
+    merge_list = f"Showing the last {mrs_to_show} merge requests" + '\n' + '\n'
+
+    merge_list = merge_list + f"Frontend:" + '\n' + '\n'
+
+    for commit in merge_commits_front:
         message = commit.message
         if "Merge branch" in message:
             branch_name = message.split("'")[1]
             merge_list = merge_list + "    " + branch_name + '\n'
-    
+
+    backend = git.Repo(local_settings.LOCAL_REPO_BACK)
+    merge_commits_back = backend.iter_commits(
+        'HEAD', max_count=mrs_to_show, merges=True)
+    merge_list = merge_list + '\n' f"Backend:" + '\n' + '\n'
+
+    for commit in merge_commits_back:
+        message = commit.message
+        if "Merge branch" in message:
+            branch_name = message.split("'")[1]
+            merge_list = merge_list + "    " + branch_name + '\n'
+
     return merge_list
-
-
